e369e4f01608e033be5deab4bd85648b72972c81
leesh9098 <leesh9098@naver.com>
2026-05-10 22:03:52 +0900

    26.05.10.1 react router 초기 기본 설정

--- app/root.tsx
+++ app/root.tsx
@@ -1,14 +1,21 @@
 import {
   isRouteErrorResponse,
+  Link,
   Links,
   Meta,
   Outlet,
+  redirect,
   Scripts,
   ScrollRestoration,
 } from "react-router";
 
 import type { Route } from "./+types/root";
 import "./app.css";
+import { getSession } from "./lib/sessions.server";
+import { getMe } from "./lib/apis/user";
+import { userContext } from "./context/userContext";
+import { Sidebar, SidebarContent, SidebarMenu, SidebarMenuButton, SidebarMenuItem, SidebarProvider } from "./components/ui/sidebar";
+import Header from "./components/organisms/Header";
 
 export const links: Route.LinksFunction = () => [
   { rel: "preconnect", href: "https://fonts.googleapis.com" },
@@ -23,9 +30,38 @@ export const links: Route.LinksFunction = () => [
   },
 ];
 
+async function authMiddleware({ request, context }: Route.LoaderArgs) {
+  const pathname = new URL(request.url).pathname;
+  const cookie = request.headers.get("Cookie");
+  const session = await getSession(cookie);
+  const token = session.get("token");
+  if (!token) {
+    context.set(userContext, null);
+    return;
+  } else if (
+    pathname === '/login' ||
+    pathname === '/register' ||
+    pathname === '/auth/verify-email'
+  ) {
+    throw redirect('/');
+  }
+  const user = await getMe({ token });
+  context.set(userContext, user);
+}
+
+export const middleware: Route.MiddlewareFunction[] = [
+  async (_, next) => {
+    const start = performance.now();
+    await next();
+    const duration = performance.now() - start;
+    console.log(`Navigation took ${duration}ms`);
+  },
+  authMiddleware
+];
+
 export function Layout({ children }: { children: React.ReactNode }) {
   return (
-    <html lang="en">
+    <html lang="ko">
       <head>
         <meta charSet="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
@@ -33,7 +69,33 @@ export function Layout({ children }: { children: React.ReactNode }) {
         <Links />
       </head>
       <body>
-        {children}
+        <SidebarProvider className="flex-col">
+          {children}
+          <Sidebar
+            side="right"
+            variant="floating"
+            className="md:hidden"
+          >
+            <SidebarContent className="py-4 px-2">
+              <SidebarMenu>
+                <SidebarMenuItem>
+                  <SidebarMenuButton>
+                    <Link to="/search">
+                      <span className="text-lg font-semibold">고인 검색</span>
+                    </Link>
+                  </SidebarMenuButton>
+                </SidebarMenuItem>
+                <SidebarMenuItem>
+                  <SidebarMenuButton>
+                    <Link to="/family-groups">
+                      <span className="text-lg font-semibold">가족 그룹</span>
+                    </Link>
+                  </SidebarMenuButton>
+                </SidebarMenuItem>
+              </SidebarMenu>
+            </SidebarContent>
+          </Sidebar>
+        </SidebarProvider>
         <ScrollRestoration />
         <Scripts />
       </body>
@@ -41,8 +103,20 @@ export function Layout({ children }: { children: React.ReactNode }) {
   );
 }
 
-export default function App() {
-  return <Outlet />;
+export async function loader({ context }: Route.LoaderArgs) {
+  const user = context.get(userContext);
+  return { user };
+}
+
+export default function App({ loaderData }: Route.ComponentProps) {
+  const { user } = loaderData;
+
+  return (
+    <>
+      <Header user={user} />
+      <Outlet />
+    </>
+  )
 }
 
 export function ErrorBoundary({ error }: Route.ErrorBoundaryProps) {
