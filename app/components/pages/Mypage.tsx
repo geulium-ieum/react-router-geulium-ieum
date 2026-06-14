f95fc6161155c00f2003a8ccb607be8b62e1f892
LimHaeJun <6462jun@gmail.com>
2026-06-14 21:49:15 +0900

    공지사항 페이지 생성

--- app/components/pages/Mypage.tsx
+++ app/components/pages/Mypage.tsx
@@ -1,7 +1,17 @@
 export default function Mypage() {
     return (
-        <div>
-            <h1>마이페이지</h1>
+        <div
+            className = "bg-gray-50"
+        >
+            <div
+                className = "max-w-7xl min-h-[calc(100vh-398px)] mx-auto px-4 sm:px-6 lg:px-8 py-12"
+            >
+                <div
+                    className = "mb-8 flex items-center justify-between"
+                >
+                    <h1 className="text-3xl text-gray-900 mb-2">마이페이지</h1>
+                </div>
+            </div>
         </div>
     )
 }
