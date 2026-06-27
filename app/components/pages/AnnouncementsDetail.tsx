fdb3219fffbc0a896679efc374bb51eea842b820
LimHaeJun <6462jun@gmail.com>
2026-06-27 21:58:51 +0900

    공지사항 목록 페이지 수정(완료)

--- app/components/pages/AnnouncementsDetail.tsx
+++ app/components/pages/AnnouncementsDetail.tsx
@@ -1,6 +1,7 @@
 import { useLocation, Navigate, Link } from "react-router";
 import { useParams } from "react-router";
 import { Card, CardContent, CardHeader, CardTitle } from "~/components/ui/card";
+import type { Route } from "./+types/AnnouncementsDetail";
 
 interface Announcement {
   id: string;
@@ -13,34 +14,33 @@ interface Announcement {
 
 export default function AnnouncementsDetail() {
   const location = useLocation();
-  const state = location.state as
-    | { announcement?: Announcement; announcements?: Announcement[] }
-    | null;
+  const state = location.state as {
+    announcement?: Announcement;
+    announcements?: Announcement[];
+  } | null;
 
   const { announcements } = state ?? {};
   const params = useParams();
-  const announcement = announcements?.find(a => a.id === params.id);
+  const announcement = announcements?.find((a) => a.id === params.id);
 
   if (!announcement) {
     return <Navigate to="/announcements" replace />;
   }
 
   return (
-    <div
-      className="bg-gray-50"
-    >
-      <div
-        className="max-w-7xl min-h-[calc(100vh-398px)] mx-auto px-4 sm:px-6 lg:px-8 py-12"
-      >
-        <Card
-            className="flex-1 flex flex-col"
-        >
-            <CardHeader>
-                <CardTitle className="text-2xl text-gray-900 font-bold">{announcement.title}</CardTitle>
-            </CardHeader>
-            <CardContent>
-                <p className="text-gray-700 whitespace-pre-wrap">{announcement.content}</p>
-            </CardContent>
+    <div className="bg-gray-50">
+      <div className="max-w-7xl min-h-[calc(100vh-398px)] mx-auto px-4 sm:px-6 lg:px-8 py-12">
+        <Card className="flex-1 flex flex-col">
+          <CardHeader>
+            <CardTitle className="text-2xl text-gray-900 font-bold">
+              {announcement.title}
+            </CardTitle>
+          </CardHeader>
+          <CardContent>
+            <p className="text-gray-700 whitespace-pre-wrap">
+              {announcement.content}
+            </p>
+          </CardContent>
         </Card>
       </div>
     </div>
