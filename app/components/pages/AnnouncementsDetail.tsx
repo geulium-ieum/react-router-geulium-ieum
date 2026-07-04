cdd536b91148b5d5238c459b2b8b83f12ac0a148
LimHaeJun <6462jun@gmail.com>
2026-07-04 22:19:52 +0900

    공지사항 작성자 이름 추가, 시간 형식 변경(완료), 시간 계산(수정중)

--- app/components/pages/AnnouncementsDetail.tsx
+++ app/components/pages/AnnouncementsDetail.tsx
@@ -3,6 +3,7 @@ import { useParams } from "react-router";
 import { Card, CardContent, CardHeader, CardTitle } from "~/components/ui/card";
 import type { Route } from "./+types/AnnouncementsDetail";
 import { Button } from "~/components/ui/button";
+import moment from "moment";
 
 interface Announcement {
   id: string;
@@ -14,7 +15,34 @@ interface Announcement {
   createdAt: string;
   updatedAt: string;
   isPinned: boolean;
+  authorName: string;
 }
+
+// function elapsedTime(date) {
+//   const start = new Date(date);
+//   const end = new Date();
+
+//   const diff = (end - start) / 1000;
+
+//   const times = [
+//     { name: '년', milliSeconds: 60 * 60 * 24 * 365 },
+//     { name: '개월', milliSeconds: 60 * 60 * 24 * 30 },
+//     { name: '일', milliSeconds: 60 * 60 * 24 },
+//     { name: '시간', milliSeconds: 60 * 60 },
+//     { name: '분', milliSeconds: 60 },
+//   ];
+
+//   for (const value of times) {
+//     const betweenTime = Math.floor(diff / value.milliSeconds);
+
+//     if (betweenTime > 0) {
+//       return `${betweenTime}${value.name} 전`;
+//     }
+//   }
+//   return '방금 전';
+// }
+
+// elapsedTime('2022-11-15');
 
 export default function AnnouncementsDetail() {
   const location = useLocation();
@@ -38,11 +66,16 @@ export default function AnnouncementsDetail() {
           <h1 className="text-3xl text-gray-900 mb-4">공지사항</h1>
         </div>
         <Card className="mb-2 flex-1 flex flex-col">
-          <CardHeader className="mb-8 flex items-center justify-between">
+          <CardHeader className="flex items-center justify-between">
             <CardTitle className="text-2xl text-gray-900 font-bold">
               {announcement.title}
             </CardTitle>
-            <span className="text-gray-700">{announcement.createdAt}</span>
+            <div className="justify-end">
+              <p className="text-gray-700">{announcement.authorName}</p>
+              <p className="text-gray-700">
+                {moment(announcement.createdAt).format("YY-MM-DD HH-mm")}
+              </p>
+            </div>
           </CardHeader>
           <CardContent>
             <p className="text-lg text-gray-700 whitespace-pre-wrap">
