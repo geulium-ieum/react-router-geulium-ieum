d800dd65088d127f3283c687d6d81218bc056aa3
LimHaeJun <6462jun@gmail.com>
2026-07-26 22:02:36 +0900

    가족그룹 join api 기능 추가

--- app/components/pages/familyGroup/Join.tsx
+++ app/components/pages/familyGroup/Join.tsx
@@ -1,11 +1,49 @@
 import type { Route } from "./+types/Join";
+import { Spinner } from "~/components/ui/spinner";
+import { userContext } from "~/context/userContext";
+import { getSession } from "~/lib/sessions.server";
+import { redirect } from "react-router";
+import { useState } from "react";
+import { familyGroupService } from "~/lib/services/familyGroup";
 
 export async function loader({ request, context }: Route.LoaderArgs) {
+  const user = context.get(userContext);
+  const cookie = request.headers.get("Cookie");
+  const session = await getSession(cookie);
+  const token = session.get("token");
   const url = new URL(request.url);
   const query = url.searchParams.get("id");
   console.log("query", query);
+  if (!user || !token) {
+    return redirect("/login");
+  }
+}
+
+export async function action({ request, context }: Route.ActionArgs) {
+  const user = context.get(userContext);
+  const cookie = request.headers.get("Cookie");
+  const session = await getSession(cookie);
+  const token = session.get("token");
+  const url = new URL(request.url);
+  const id = url.searchParams.get("id");
+  if (!user || !token) {
+    return redirect("/login");
+  }
+  if (!id) {
+    return redirect("/");
+  }
+  try {
+    await familyGroupService.post.joinFamilyGroup({
+      id,
+      token,
+    });
+    return redirect("/");
+  } catch (error) {
+    console.log(error);
+  }
 }
 
 export default function Join() {
-  return null;
+  const [isLoading, setIsLoading] = useState(false);
+  return <div>{isLoading && <Spinner />}</div>;
 }
